9cff19c0de79e80e755f5ab52d17e30182a4a266
Ariel <ariel.rossi78@gmail.com>
2023-05-20 17:12:24 -0300

    agregue email y la action formspree

--- Javascript/formcontact.js
+++ Javascript/formcontact.js
@@ -1,6 +1,5 @@
+
 //validacion de formulario de contacto
-
-
 function valida_envia(){
     //valido el nombre
     if (document.fvalida.nombre.value.length==0){
@@ -9,11 +8,18 @@ function valida_envia(){
           return 0;
         }
         //valido el apellido
-    if (document.fvalida.apellido.value.length==0){
+      if (document.fvalida.apellido.value.length==0){
           alert("Tiene que escribir su apellido")
           document.fvalida.apellido.focus()
           return 0;
         }
+
+         //valido el email
+      if (document.fvalida.email.value.length==0){
+      alert("Tiene que escribir su e-mail")
+      document.fvalida.email.focus()
+      return 0;
+      }
         //valido su codigo postal
         if (document.fvalida.direccion.value.length==0){
           alert("Tiene que escribir su direccion")
