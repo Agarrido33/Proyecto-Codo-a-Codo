930c59c0360a08c4b098e296463bc1adb0b45b40
Ariel <ariel.rossi78@gmail.com>
2023-05-21 00:02:41 -0300

    agregue campos en javacontactos

--- Javascript/formcontact.js
+++ Javascript/formcontact.js
@@ -1,38 +1,74 @@
+//validacion de email
+function valida_envia()
+const form  = document.getElementsByTagName('form')[0];
 
-//validacion de formulario de contacto
-function valida_envia(){
-    //valido el nombre
-    if (document.fvalida.nombre.value.length==0){
-          alert("Tiene que escribir su nombre")
-          document.fvalida.nombre.focus()
-          return 0;
-        }
-        //valido el apellido
-      if (document.fvalida.apellido.value.length==0){
-          alert("Tiene que escribir su apellido")
-          document.fvalida.apellido.focus()
-          return 0;
-        }
+const email = document.getElementById('mail');
+const emailError = document.querySelector('#mail + span.error');
 
-         //valido el email
-      if (document.fvalida.email.value.length==0){
-      alert("Tiene que escribir su e-mail")
-      document.fvalida.email.focus()
-      return 0;
-      }
-        //valido su codigo postal
-        if (document.fvalida.direccion.value.length==0){
-          alert("Tiene que escribir su direccion")
-          document.fvalida.codigopostal.focus()
-          return 0;
-        }
-        //valido su direccion
-        if (document.fvalida.direccion.value.length==0){
-          alert("Tiene que escribir su direccion")
-          document.fvalida.direccion.focus()
-          return 0;
-        }
-        //el formulario se envia
-        alert("Muchas gracias por enviar el formulario");
-        document.fvalida.submit();
+email.addEventListener('input', function (event) {
+  // Cada vez que el usuario escribe algo, verificamos si
+  // los campos del formulario son válidos.
+
+  if (email.validity.valid) {
+    // En caso de que haya un mensaje de error visible, si el campo
+    // es válido, eliminamos el mensaje de error.
+    emailError.innerHTML = ''; // Restablece el contenido del mensaje
+    emailError.className = 'error'; // Restablece el estado visual del mensaje
+  } else {
+    // Si todavía hay un error, muestra el error exacto
+    showError();
+  }
+});
+
+form.addEventListener('submit', function (event) {
+  // si el campo de correo electrónico es válido, dejamos que el formulario se envíe
+
+  if(!email.validity.valid) {
+    // Si no es así, mostramos un mensaje de error apropiado
+    showError();
+    // Luego evitamos que se envíe el formulario cancelando el evento
+    event.preventDefault();
+  }
+});
+
+function showError() {
+  if(email.validity.valueMissing) {
+    // Si el campo está vacío
+    // muestra el mensaje de error siguiente.
+    emailError.textContent = 'Debe introducir una dirección de correo electrónico.';
+  } else if(email.validity.typeMismatch) {
+    // Si el campo no contiene una dirección de correo electrónico
+    // muestra el mensaje de error siguiente.
+    emailError.textContent = 'El valor introducido debe ser una dirección de correo electrónico.';
+  } else if(email.validity.tooShort) {
+    // Si los datos son demasiado cortos
+    // muestra el mensaje de error siguiente.
+    emailError.textContent = 'El correo electrónico debe tener al menos ${ email.minLength } caracteres; ha introducido ${ email.value.length }.';
+  }
+
+  // Establece el estilo apropiado
+  emailError.className = 'error activo';
+}
+
+
+
+//para que no salga el formspree
+const $form = document.querySelector('#form')
+
+$form.addeventlistener('submit', handlesubmit)
+
+   async function handlesubmit(event) {
+  event.preventDefauls()
+  const form = new FormData(this)
+  const response = await fetch(this.action, {
+    method: this.method,
+    body: form,
+    headers: {
+      'Accept': 'application/json'
     }
+  })
+  if (response.ok) { 
+    this.reset()
+    alert("Muchas gracias por consultar, le respondonderemos a la brevedad :)")
+  }
+}
